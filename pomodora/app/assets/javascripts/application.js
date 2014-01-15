de384fa94744dba9d4624eacec0240f673ae3fbe
Apprentice <apprentice@dbc20.local>
2014-01-14 17:42:44 -0800

    first commit

---
+++ pomodora/app/assets/javascripts/application.js
@@ -0,0 +1,49 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
+
+console.log("this is hitttt")
+
+(function($) {
+
+  $.fn.addSliderSegments = function (amount) {
+    return this.each(function () {
+      var segmentGap = 100 / (amount - 1) + "%"
+        , segment = "<div class='ui-slider-segment' style='margin-left: " + segmentGap + ";'></div>";
+      $(this).prepend(segment.repeat(amount - 2));
+    });
+  };
+  var $slider = $("#slider");
+    if ($slider.length) {
+      $slider.slider({
+        min: 1,
+        max: 5,
+        value: 2,
+        orientation: "horizontal",
+        range: "min"
+      }).addSliderSegments($slider.slider("option").max);
+    }
+  console.log("this is being hit at least")
+  var button = $('button')
+  button.on("click", function() {
+    console.log("this is hit")
+    var useritem = $('#textbox').val()
+    console.log(useritem)
+    $('.completed').val(useritem)
+    $('.completed').attr("display", "visible")
+
+  })
+
+})
